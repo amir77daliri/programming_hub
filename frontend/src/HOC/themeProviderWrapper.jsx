67c250a2c2ed0bc3dd871584880285a7ba2b7c26
Amirhossein_Daliri <amirdalir0111@gmail.com>
2023-12-24 15:22:52 +0330

    start frontend with vite and MUI / rtl mui

---
+++ frontend/src/HOC/themeProviderWrapper.jsx
@@ -0,0 +1,28 @@
+// RTL :
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import rtlPlugin from 'stylis-plugin-rtl'
+import { CacheProvider } from '@emotion/react'
+import createCache from '@emotion/cache'
+import { prefixer } from 'stylis'
+
+const theme = createTheme({
+    direction: "rtl"
+})
+
+const cacheRTL = createCache({
+    key: 'muirtl',
+    stylisPlugins: [prefixer, rtlPlugin]
+})
+
+
+const ThemeProviderWrapper = ({children}) => {
+    return (
+        <CacheProvider value={cacheRTL} >
+            <ThemeProvider theme={theme}>
+                {children}
+            </ThemeProvider>
+        </CacheProvider>
+    )
+}
+
+export default ThemeProviderWrapper;
